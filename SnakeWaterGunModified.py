896d618084a8174e99036a1585b22d7d3d28dca4
sk2412840 <74950699+sk2412840@users.noreply.github.com>
2020-12-08 17:52:54 +0530

    Add files via upload

---
+++ SnakeWaterGunModified.py
@@ -0,0 +1,62 @@
+import random
+import sys
+
+choicelist= ["Snake", "Water", "Gun"]
+
+while True:
+    player = input("Enter s for Snake, w for Water or g for Gun :\n")
+    if player == "s":
+        player = "Snake"
+    elif player == "w":
+        player = "Water"
+    elif player == "g":
+        player = "Gun"
+    else:
+        print("Invalid Input")
+        sys.exit()
+
+    if player in choicelist:
+        computer = random.choice(choicelist)
+        if player == "Snake" and computer == "Snake":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("It's a Draw")
+        elif player == "Snake" and computer == "Water":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("You Win")
+        elif player == "Snake" and computer == "Gun":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("Computer Win")
+        elif player == "Water" and computer == "Snake":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("Computer Win")
+        elif player == "Water" and computer == "Water":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("It's a Draw")
+        elif player == "Water" and computer == "Gun":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("You Win")
+        elif player == "Gun" and computer == "Snake":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("You Win")
+        elif player == "Gun" and computer == "Water":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("Computer Win")
+        elif player == "Gun" and computer == "Gun":
+            print(f"Your Choice is {player} and computer's choice is {computer}")
+            print("It's a Draw")
+        else:
+            sys.exit()
+
+    playagain= input("Do You Want To Play Again y\\n:\n")
+    if playagain== "y":
+        continue
+    elif playagain== "n":
+        print("Thank You For Playing")
+        break
+    else:
+        sys.exit()
+
+
+
+
+
