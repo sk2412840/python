0f5433153c24a6732539014e3a21768cc9c91fd3
sk2412840 <74950699+sk2412840@users.noreply.github.com>
2020-12-08 17:59:51 +0530

    Add files via upload

---
+++ np-1.py
@@ -0,0 +1,10 @@
+import numpy as np
+arr=np.zeros(10)
+print("An array of 10 zeros is:")
+print(arr)
+arr=np.ones(10)
+print("An array of 10 ones is:")
+print(arr)
+arr=np.ones(10)*5
+print("An array of 10 fives is:")
+print(arr)
